1d6929ae23c7b12d13609f6859971e1c006f3c82
bettyabnet <betelhemabnet1@gmail.com>
2024-04-11 00:55:50 +0300

    resolve

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,8 @@
-function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve(true);
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
   });
 }
+
+export default getFullResponseFromAPI;
